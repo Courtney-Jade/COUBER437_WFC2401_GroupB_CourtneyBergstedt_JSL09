108a21c96a2936694822e744167c873cb7994d12
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-04-10 15:48:50 +0200

    updated js and css

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_JSL09/index.js
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_JSL09/index.js
@@ -54,8 +54,8 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
                 const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
                 document.getElementById("weather").innerHTML = `
                     <img src=${iconUrl} />
-                    <p>${Math.round(data.main.temp)}º</p>
-                    <p>${data.name}</p>
+                    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                    <p class="weather-city">${data.name}</p>
                 `
             })
             .catch(err => console.error(err))
